8a46629f7519f0db5960d3a1d08f23929bcbc463
tffragoso <tffragoso@gmail.com>
2024-05-15 19:56:05 +0100

    xml document

---
+++ src/main/kotlin/XmlDocument.kt
@@ -0,0 +1,14 @@
+class XmlDocument(
+
+    val name: String,
+    var config : XmlLeaf,
+    var body : XmlTag
+)  {
+
+    init {
+        config = XmlLeaf("xml")
+        config.addAttribute(Attribute("version", "1.0"))
+        config.addAttribute(Attribute("encoding", "UTF-8"))
+    }
+
+}
