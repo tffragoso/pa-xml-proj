4fa88b15585e8dd2cc27387c5b461575a2da5d28
tffragoso <tffragoso@gmail.com>
2024-05-27 10:57:28 +0100

    minor fixes

--- src/main/kotlin/XmlLeaf.kt
+++ src/main/kotlin/XmlLeaf.kt
@@ -5,8 +5,6 @@ import kotlin.reflect.full.hasAnnotation
 data class XmlLeaf(
     override var name: String,
     override var parent: XmlTag? = null,
-    //TODO
-    //private val attributes
     override var attributes: MutableList<XmlAttribute> = mutableListOf(),
     val leafText: String? = null,
 ) : XmlElement {
